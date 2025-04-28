75385bd899c6112037969c8878a22e3432863883
sravankumar1199 <sravankumartudi99@gmail.com>
2025-04-28 13:10:03 +0530

    Update array.sh

--- array.sh
+++ array.sh
@@ -9,5 +9,3 @@ echo "Second Person: ${Persons[1]}"
 echo "Third Perosn: ${Perosns[3]}"
 
 echo "all: ${Persons[@]}"
-
-###### Array Starts Form 0 for example a=123456789 and b=123456789 this is called array topic #####
